2c09a7c2de9ef41af17f7c3767f7dbd61963f2e0
purnashanmugam <purnashanmugam@gmail.com>
2024-12-06 20:14:22 +0530

    Fixing issues

--- target-group.tf
+++ target-group.tf
@@ -14,7 +14,7 @@ resource "aws_lb_target_group" "target-group" {
 resource "aws_lb_listener" "alb_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = "80"
-  protocol          = "HTTPS"
+  protocol          = "HTTP"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.target-group.arn
