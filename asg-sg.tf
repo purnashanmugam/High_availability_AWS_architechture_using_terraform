6fa431a5c87a513dde16f88a5d9cff02e50c9054
purnashanmugam <purnashanmugam@gmail.com>
2024-12-06 22:44:14 +0530

    Fixing issues

--- asg-sg.tf
+++ asg-sg.tf
@@ -8,3 +8,16 @@ resource "aws_security_group" "asg_security_group" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "asg_ingress_rule" {
+  security_group_id = aws_security_group.asg_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "asg_egress_rule" {
+  security_group_id = aws_security_group.asg_security_group.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
